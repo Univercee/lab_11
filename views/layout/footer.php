42c3729c7da7769671743afb5240173b93d541d7
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-11-24 14:06:03 +0400

    update layout

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,5 +1,31 @@
-<footer class="py-5" style="background-color: #353433; color: whitesmoke;">
+<footer class="py-5" style="background-color: #F7F7F7;">
     <div class="container-xl">
-        
+        <div class="row" style="font-size: 15px;">
+            <div class="col">
+                <h6>Поддержка</h6>
+                <div><a>Центр помощи</a></div>
+                <div><a>AirCover</a></div>
+                <div><a>Борьба с дискриминацией</a></div>
+                <div><a>Помощь людям с инвалидностью</a></div>
+                <div><a>Отмена в период пандемии</a></div>
+                <div><a>Сообщить о проблеме в районе</a></div>
+            </div>
+            <div class="col">
+                <h6>Прием гостей</h6>
+                <div><a>Сдайте жилье на Airbnb</a></div>
+                <div><a>AirCover для хозяев</a></div>
+                <div><a>Материалы для хозяев</a></div>
+                <div><a>Форум сообщества</a></div>
+                <div><a>Ответственный прием гостей</a></div>
+            </div>
+            <div class="col">
+                <h6>Airbnb</h6>
+                <div><a>Пресс-центр</a></div>
+                <div><a>Новые функции</a></div>
+                <div><a>Карьера в Airbnb</a></div>
+                <div><a>Для инвесторов</a></div>
+                <div><a>Прием гостей на Airbnb.org</a></div>
+            </div>
+        </div>
     </div>
 </footer>
